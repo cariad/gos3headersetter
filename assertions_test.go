ed0b2d5ae2f6f5765c0534a4c0a2d43dc1d10515
Cariad Eccleston <cariad@cariad.me>
2019-10-15 14:49:05 +0100

    Add assertNilStringPtr()

--- assertions_test.go
+++ assertions_test.go
@@ -15,3 +15,9 @@ func assertEqualString(t *testing.T, name string, actual string, expect string) 
 		t.Errorf("expected %s to be \"%s\" but is \"%s\"", name, expect, actual)
 	}
 }
+
+func assertNilStringPtr(t *testing.T, name string, actual *string) {
+	if actual != nil {
+		t.Errorf("expected %s to be nil but is \"%s\"", name, *actual)
+	}
+}
